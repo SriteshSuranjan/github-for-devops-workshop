0561794fd55e9823b37e29e8f7f6ccc735820e92
SriteshSuranjan <sriteshsuranjan@gmail.com>
2025-02-05 11:28:59 +0000

    Added test

--- testing.py
+++ testing.py
@@ -1,6 +1,8 @@
 """
 This module contains a simple function that returns a greeting.
 """
+
+
 def hello():
     """
     Returns a greeting message.
@@ -8,5 +10,7 @@ def hello():
     str: The greeting message.
     """
     return "Hello Dosto"
+
+
 if __name__ == "__main__":
     print(hello())  # Ensures the function runs only when executed directly
